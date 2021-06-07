553510f67e77bfa6ce218fadc8f1bca3f17a1c85
Sayantan Basu <sayantanbasu@live.in>
2021-06-07 15:39:20 -0400

    updated express_server.js

---
+++ express_server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const urlDatabase = {
+  "b2xVn2":"http://www.lighthouselabs.ca",
+  "9sm5xK":"http://www/google.ca"
+};
+app.get("/", (request, response)=>{
+  response.send("hello");
+});
+
+app.listen(PORT, ()=>{
+console.log("App is listening on port: ",PORT)
+});
