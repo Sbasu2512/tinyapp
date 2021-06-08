f84baf7bf76d4f384f09461a10c990411c11a0b3
Sayantan Basu <sayantanbasu@live.in>
2021-06-08 15:49:28 -0400

    updated the file to redirect any requrest to shortURL

--- express_server.js
+++ express_server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(require('body-parser').json());
+app.use(express.urlencoded({extended: true}));
+//app.use(require('body-parser').json());
 app.set("view engine", "ejs");
 const PORT = 8080;
 //short URL: long URL
@@ -29,18 +29,22 @@ const templateVars = {urls: urlDatabase};
 res.render("urls_index", templateVars); //file name in views, the data to show on the webpage
 });
 
+//Update your express server so that the shortURL-longURL key-value pair are saved to the urlDatabase when it receives a POST request to /urls
 app.post("/urls", (req, res) => {
-  const randomString = generateRandomString();
-  console.log(req.body);  // Log the POST request body to the console
-  urlDatabase.randomString = req.body;  
-  res.send("Being redirected");  
-  res.redirect(`/urls/:${randomString}`)       
-  
+  const shortURL = generateRandomString();
+  // console.log(req.body);  // Log the POST request body to the console
+  urlDatabase[shortURL] = req.body.longURL;    
+  res.redirect(`/urls/${shortURL}`);       
 });
 
-app.get(`/urls/:shortURL`, (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  res.render(`urls_show`, templateVars);
+app.get("/urls/new", (req, res)=>{
+  //console.log("Testing here.")
+  res.render("urls_new");
+});
+//req.params => u can access the variables within your routes.
+app.get("/urls/:shortURL", (req,res)=>{
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
+  res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -48,14 +52,6 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-app.get("urls/new", (req, res)=>{
-  res.render("urls_new");
-  });
-
-app.get("/urls/:shortURL", (req,res)=>{
-const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
-res.render("urls_show", templateVars);
-});
 
 function generateRandomString() {
 return Math.random().toString(36).substring(7);
