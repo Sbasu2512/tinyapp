b7fc669ad2f409867b7bb023f4964591a7ebd5aa
Sayantan Basu <sayantanbasu@live.in>
2021-06-07 22:02:47 -0400

    updated express_server.js: added body parser

--- express_server.js
+++ express_server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 const PORT = 8080;
 //short URL: long URL
