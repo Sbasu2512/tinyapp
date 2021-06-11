e17e188e9cb1640a1e3434eca52e938afe38822b
Sayantan Basu <sayantanbasu@live.in>
2021-06-11 02:17:23 -0400

    added more fns

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -3,30 +3,26 @@ const { assert } = require('chai');
 const { urlsForUser } = require('../helper');
 
 describe('urlsForUser', function() {
-  it('should return true if the user exsits in the database', function() {
-    const user = urlsForUser('userRandomID', database);
-    console.log(user);
-    const expectedOutput = true;
+  it('should return the id of which the user exsits in the database', function() {
+    const user = urlsForUser('aJ48lW', database);
+    console.log("value of user is ",user);
+    const expectedOutput = {b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" }};
     // Write your assert statement here
-    assert.equal(user,expectedOutput);
+    assert.deepEqual(user,expectedOutput);
+   
   });
 
-  it('should return false if the user does not exsits in the database', function() {
+  it('should return an empty object if the userid does not exsits in the database', function() {
     const user = urlsForUser('user3RandomID', database)
-    const expectedOutput = false;
+   // console.log(user);
+    const expectedOutput = {};
     // Write your assert statement here
-    assert.equal(user,expectedOutput);
+    assert.deepEqual(user,expectedOutput);
   });
 });
 
 const database = {
-  "userRandomID": {
-    userRandomID: 'www.facebook.com',
-    userRandomID: 'www.yahoo.com'
-  },
-  "user2RandomID": {
-    user2RandomID: "www.bitcoin.com", 
-    user2RandomID: "www.wwe.com"
-  }
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "bJ48lU" }
 }
 
