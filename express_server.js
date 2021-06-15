8ff42b0899ac3d89da7f34ccb0a98f0ab8f3decd
Sayantan Basu <sayantanbasu@live.in>
2021-06-14 20:57:54 -0400

    edited /user route so that it does not show info to anybody

--- express_server.js
+++ express_server.js
@@ -127,7 +127,6 @@ app.get("/u/:shortURL", (req, res) => {
   let user = users[req.session.userid];
   if (user) {
     if (user.id === ownedURLs[req.params.shortURL].userID) {
-      /*  */
       const longURL = urlDatabase[req.params.shortURL].longURL;
       return res.redirect(longURL);
     }
@@ -152,7 +151,10 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/user", (req, res)=>{
-  res.render("user",{users});
+  if(user && userLogin.email === "admin" &&  bcrypt.compareSync("admin", userLogin.password)){
+ return res.render("user",{users});
+}
+return res.redirect("/login")
 })
 // allows user to login with a username - redirects to /urls
 app.post("/login", (req, res) => {
@@ -180,9 +182,6 @@ app.post("/login", (req, res) => {
       break;
     }
   }
- // console.log('expecting users[id].user to appear here: 183 ',user);
-  //console.log('expecting users[id].password to appear here: 184 ',user.password);
-  //let doesPasswordMatch = bcrypt.compareSync(userLogin.password, user.password);
   // if user exists & password matches
   if (user && bcrypt.compareSync(userLogin.password, user.password)) {
       //this log will appear in the server terminal, NOT on the browser
@@ -199,26 +198,20 @@ app.post("/login", (req, res) => {
 
 // allows users to logout
 app.post("/logout", (req, res) => {
-  //res.clearCookie("userid");
   req.session['userid'] = null ;
-  //console.log("cookie cleared");
   res.redirect("/login");
-  //console.log("logged out");
 });
 
 //which returns the template regitration.ejs
 app.get("/register", (req, res) => {
-  //we are getting the userid cookie. name should be same all over
   let user = users[req.session.userid];
-  //console.log("req.cookies.userid line 159 :", req.cookies.userid);
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
     userid: req.session.userid,
-    user: user, //user = users[req.cookies.userid]
+    user: user, 
   };
-  //resgitration == view, templateVars == local object for this specific view
-  res.render("registration", templateVars); //res.render(view [, locals] [, callback])
+  res.render("registration", templateVars); 
 });
 
 //Registering New Users
@@ -226,32 +219,25 @@ app.post("/register", (req, res) => {
   const errorMsg = "User email exists, please login";
   const userId = generateRandomString();
   const newUser = {
-    id: userId, //create a new object so we do not have to go....
-    email: req.body.email, //....two levels deep of the get go.
+    id: userId, 
+    email: req.body.email, 
     password: bcrypt.hashSync(req.body.password, saltRounds),
   };
   let user;
   userLogin = req.body;
   for (let id in users) {
     if (users[id].email === userLogin.email) {
-     // console.log(users[id]);
-      user = users[id]; //id = generateRandomString()
+      user = users[id]; 
       break;
     }
   }
   if (user) {
     // if email matches
     if (user.email === userLogin.email) {
-      //this log will appear in the server terminal, NOT on the browser
-      //console.log("user already exists");
-      // redirect to homepage
-      // early return to stop the function
       return res.status(403).send(errorMsg);
     }
   }
   users[userId] = newUser;
-  // console.log(userId);
-  //res.cookie("userid", userId); //set a cookie with name(key), value
   req.session['userid'] = userId
   res.redirect("/urls");
 });
