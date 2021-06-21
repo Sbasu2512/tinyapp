1b637876cf79ecb6304f6be65c9f89a7b6d64083
Sayantan Basu <sayantanbasu@live.in>
2021-06-21 13:23:30 -0400

    removing unused code

--- routes/functionality.js
+++ routes/functionality.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const cookieParser = require("cookie-parser");
-const cookieSession = require("cookie-session");
+//const cookieParser = require("cookie-parser");
+//const cookieSession = require("cookie-session");
 const { urlsForUser, generateRandomString, emailLooker } = require("../helper");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+//const bcrypt = require("bcrypt");
+//const saltRounds = 10;
 
 const func = (users, urlDatabase) => {
 // for creating new shortURLs
