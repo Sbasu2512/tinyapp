e76686c8ae9488f0496943d2ed6aa943647623e8
Sayantan Basu <sayantanbasu@live.in>
2021-06-21 13:22:20 -0400

    a file with global objects: urlDatabase & users

---
+++ routes/database.js
@@ -0,0 +1,6 @@
+//Create a global object called users which will be used to store and access the users in the app.
+const users = {};
+//URL database
+const urlDatabase = {};
+
+module.exports = {users, urlDatabase};
