03538029e04e07ce1cdabf4e1610b96d01a6fef4
Sayantan Basu <sayantanbasu@live.in>
2021-06-22 13:04:56 -0400

    formatting for better readability

--- express_server.js
+++ express_server.js
@@ -1,12 +1,12 @@
 //package imports
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cookieSession = require('cookie-session');
+const cookieSession = require("cookie-session");
 //local imports
 const loginRoutes = require("./routes/login");
 const homeRoutes = require("./routes/home");
 const func = require("./routes/functionality");
-const {urlDatabase, users} = require('./routes/database');
+const { urlDatabase, users } = require("./routes/database");
 //constants
 const PORT = process.env.PORT || 8080; // default port 8080
 //Middlewares
@@ -14,20 +14,23 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1','key2']
-}))
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+  })
+);
 //activating my database object
 // app.use('/',database);
 //activate my user object
 // app.use('/',users);
 //activating my login routes
-app.use('/',loginRoutes(users, urlDatabase));
+app.use("/", loginRoutes(users, urlDatabase));
 // homepage (root)
-app.use('/', homeRoutes(users, urlDatabase));
+app.use("/", homeRoutes(users, urlDatabase));
 //activating func
-app.use('/', func(users, urlDatabase));
+app.use("/", func(users, urlDatabase));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
