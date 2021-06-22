acefff6a83f19af37248f2e477d84ebf4ab00d94
Sayantan Basu <sayantanbasu@live.in>
2021-06-22 13:08:54 -0400

    formatting and changing vars for better readability

--- routes/functionality.js
+++ routes/functionality.js
@@ -1,87 +1,89 @@
 //package imports
 const express = require("express");
+//middleware
 const router = express.Router();
 //local imports
-const { urlsForUser} = require("../helper");
+const { urlsForUser } = require("../helper");
 
 const func = (users, urlDatabase) => {
-// for creating new shortURLs
-router.get("/urls/new", (req, res) => {
-  let user = users[req.session.userid];
-  const templateVars = {
-    user: user,
-  };
-  if(user !== 'undefined' && user){
-    res.render("urls_new", templateVars);
-  }
-  res.redirect("/login");
-});
-// shows user their shortURL
-router.get("/urls/:shortURL", (req, res) => {
-  const ownedURLs = urlsForUser(req.session.userid, urlDatabase);
-  let shortURL = req.params.shortURL;
-  let user = users[req.session.userid];
-  //if user logged in or not check
-  if (!user) {
-    return res.send("Please Login/Register");
-  }
-  //checking if the user owns the url or not
-  if (!ownedURLs[shortURL]) {
-    return res.send("URL does not exist");
-    };  
+  // for creating new shortURLs
+  router.get("/urls/new", (req, res) => {
+    const user = users[req.session.userid];
+    //template Vars to be passed to the page we are rendering
+    const templateVars = {
+      user: user,
+    };
+    //checking if user has logged in or not
+    if (user !== "undefined" && user) {
+      res.render("urls_new", templateVars);
+    }
+    res.redirect("/login");
+  });
+  // shows user their shortURL
+  router.get("/urls/:shortURL", (req, res) => {
+    const ownedURLs = urlsForUser(req.session.userid, urlDatabase);
+    let shortURL = req.params.shortURL;
+    let user = users[req.session.userid];
+    //if user logged in or not check
+    if (!user) {
+      return res.send("Please Login/Register");
+    }
+    //checking if the user owns the url or not
+    if (!ownedURLs[shortURL]) {
+      return res.send("URL does not exist");
+    }
     let longURL = ownedURLs[req.params.shortURL].longURL;
     const templateVars = {
       shortURL,
       longURL,
       userid: req.session.userid,
       user,
-    }
-    return res.render("urls_show", templateVars);
-});
-// updates URL - longURL edited for specified shortURL
-router.post("/urls/:shortURL/update", (req, res) => {
-  const ownedURLs = urlsForUser(req.session.userid, urlDatabase);
-  let user = users[req.session.userid]; 
-  shortURL = req.params.shortURL;
-  if (!user) {
-    return res.send("Please Login/Register");
-  }
-  if (!ownedURLs[shortURL]) {
-    return res.send("URL does not exsist");
     };
+    return res.render("urls_show", templateVars);
+  });
+  // updates URL - longURL edited for specified shortURL
+  router.post("/urls/:shortURL/update", (req, res) => {
+    const ownedURLs = urlsForUser(req.session.userid, urlDatabase);
+    let user = users[req.session.userid];
+    shortURL = req.params.shortURL;
+    if (!user) {
+      return res.send("Please Login/Register");
+    }
+    if (!ownedURLs[shortURL]) {
+      return res.send("URL does not exsist");
+    }
     urlDatabase[shortURL] = {
       longURL: req.body.longURL,
-      userID: req.session.userid
+      userID: req.session.userid,
     };
-  res.redirect(`/urls/${req.params.shortURL}`);
+    res.redirect(`/urls/${req.params.shortURL}`);
+  });
+  // uses shortURL to redirect to longURL. non logged in users should be able to use it
+  router.get("/u/:shortURL", (req, res) => {
+    const longURL = urlDatabase[req.params.shortURL].longURL;
+    if (longURL.includes("http")) {
+      return res.redirect(longURL);
+    } else {
+      return res.redirect(`https://${longURL}`);
+    }
+  });
+  // remove shortURL then redirect back to /urls
+  router.post("/urls/:shortURL/delete", (req, res) => {
+    const ownedURLs = urlsForUser(req.session.userid, urlDatabase);
+    let user = users[req.session.userid]; //user id
+    let shortURL = req.params.shortURL;
+    if (!user) {
+      return res.send("Please Login/Register");
+    }
+    if (!ownedURLs[shortURL]) {
+      return res.send("URL does not exsist");
+    }
+    if (user.id === ownedURLs[shortURL].userID) {
+      delete urlDatabase[shortURL];
+    }
+    res.redirect("/urls");
+  });
 
-});
-// uses shortURL to redirect to longURL. non logged in users should be able to use it
-router.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL].longURL;
-  if (longURL.includes('http')) {
-    return res.redirect(longURL);
-  } else {
-    return res.redirect(`https://${longURL}`);
-  }
-});
-// remove shortURL then redirect back to /urls
-router.post("/urls/:shortURL/delete", (req, res) => {
-  const ownedURLs = urlsForUser(req.session.userid, urlDatabase);
-  let user = users[req.session.userid]; //user id
-  let shortURL = req.params.shortURL;
-  if (!user) {
-    return res.send("Please Login/Register");
-  }
-  if (!ownedURLs[shortURL]) {
-    return res.send("URL does not exsist");
-  }
-  if (user.id === ownedURLs[shortURL].userID) {
-    delete urlDatabase[shortURL];
-  }
-  res.redirect("/urls");
-});
-
-return router;
-}
+  return router;
+};
 module.exports = func;
